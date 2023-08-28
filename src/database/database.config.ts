639b5e94d552a40d5eddbcc4be36cc2019f09bce
mdhenriques <md.henriques@outlook.com>
2023-08-28 16:01:44 -0300

    Configurando dotenv

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -3,11 +3,11 @@ import pg from "pg";
 
 export const sequelize = new Sequelize({
     dialect: 'postgres',
-    host: 'desafio3.postgres.database.azure.com',
+    host: process.env.DB_HOST,
     port: 5432,
-    username: 'mdhenriques',
-    password: 'm*XUdjdxrcve1',
-    database: 'desafio3',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     dialectModule: pg, //Necessário para o deploy na vercel
         dialectOptions: { //Necessário para usar o servidor Postgre no Azure
           ssl: {
