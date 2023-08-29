2cec01c04ed52c46fb8f8fd228c303974769d7c9
mdhenriques <md.henriques@outlook.com>
2023-08-29 18:26:38 -0300

    passando guard na rota createItem

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { ItemService } from "./item.service";
 import { CreateItemDTO } from "./dto/createItem.dto";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller('items')
 export class ItemController {
 
     constructor(private readonly itemService: ItemService) {}
 
+    @UseGuards(AuthGuard)
     @Post()
     async createItem(@Body() createItemDTO: CreateItemDTO): Promise<any> {
-        console.log(createItemDTO);
+        
         const newItem = await this.itemService.createItem(createItemDTO);
         return { status: 'success', data: newItem }
     }
