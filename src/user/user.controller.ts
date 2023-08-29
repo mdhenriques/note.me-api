3c524dd9f65a8254dac1db871a51ae52a3fa329a
mdhenriques <md.henriques@outlook.com>
2023-08-28 23:51:08 -0300

    Criada rota para cadastro

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Post } from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { User } from "./user.entity";
 
+@Controller('/users')
 export class UserController {
 
-    
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    async createUser(@Body() user: User): Promise<any> {
+        try {
+            const response = await this.userService.create(user);
+            return response;
+        } catch (err) {
+            console.log(err);            
+        }
+    }
     
 }
