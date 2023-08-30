865904114a245284fc2a16579fed96ae6e4fff1e
mdhenriques <md.henriques@outlook.com>
2023-08-30 12:08:30 -0300

    Adicionando RatingModule ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './user/user.module';
 import { ItemModule } from './item/item.module';
+import { RatingModule } from './rating/rating.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, ItemModule],
+  imports: [DatabaseModule, UsersModule, ItemModule, RatingModule],
   controllers: [],
   providers: [],
 })
