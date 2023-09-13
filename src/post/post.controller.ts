f9e3acdd4c10c79580deb476d723e78b2a5b84ba
mdhenriques <md.henriques@outlook.com>
2023-09-13 15:37:18 -0300

    Updating createPost

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards } from "@nestjs/common";
 import { PostService } from "./post.service";
 import { CreatePostDTO } from "./dto/createPost.dto";
 import { Posts } from "./post.entity";
@@ -8,29 +8,26 @@ import { UpdatePostDTO } from "./dto/updatePost.dto";
 export class PostController {
 
     constructor(private readonly postService: PostService) {}
-
     
     @Post()
-    async createPost(@Body() createPostDTO: CreatePostDTO): Promise<any> {
+    @HttpCode(201)
+    async createPost(@Body() createPostDTO: CreatePostDTO): Promise<{ status: string, data: Posts}> {
         
         const newPost = await this.postService.createPost(createPostDTO);
-        return { status: 'success', data: newPost }        
+        return { status: 'success', data: newPost };        
     }
-
     
     @Delete(':id')
     async deletePost(@Param('id') id: number): Promise<any> {
         await this.postService.deletePostById(id);
         return 'Post deleted successfully';
     }
-
     
     @Put(':id')
     async updatePost(@Param('id') postId: number, @Body() updatePostDTO: UpdatePostDTO): Promise<any> {
         const updatedPost = await this.postService.updatePost(postId, updatePostDTO);
         return { status: 'success', data: updatedPost };
     }
-
 
     @Get('user/:userId')
     async getPostByUserId(@Param('userId') userId: number): Promise<Posts[]> {
