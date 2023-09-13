2f4b6ef58c3477ee3be62792dec684f5ee2ab43a
mdhenriques <md.henriques@outlook.com>
2023-09-13 15:31:38 -0300

    Removing auth guards

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -9,7 +9,7 @@ export class PostController {
 
     constructor(private readonly postService: PostService) {}
 
-    //@UseGuards(AuthGuard)
+    
     @Post()
     async createPost(@Body() createPostDTO: CreatePostDTO): Promise<any> {
         
@@ -17,14 +17,14 @@ export class PostController {
         return { status: 'success', data: newPost }        
     }
 
-    //@UseGuards(AuthGuard)
+    
     @Delete(':id')
     async deletePost(@Param('id') id: number): Promise<any> {
         await this.postService.deletePostById(id);
         return 'Post deleted successfully';
     }
 
-    //@UseGuards(AuthGuard)
+    
     @Put(':id')
     async updatePost(@Param('id') postId: number, @Body() updatePostDTO: UpdatePostDTO): Promise<any> {
         const updatedPost = await this.postService.updatePost(postId, updatePostDTO);
@@ -35,7 +35,5 @@ export class PostController {
     @Get('user/:userId')
     async getPostByUserId(@Param('userId') userId: number): Promise<Posts[]> {
         return await this.postService.getPostByUserId(userId);   
-    }
-
-    
+    }    
 }
