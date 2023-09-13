5af1c276e1a06419ce80dbd2e6ebc0186638ad04
mdhenriques <md.henriques@outlook.com>
2023-09-13 16:22:02 -0300

    Updating DTO. Adding ApiProperty decorator

--- src/post/dto/createPost.dto.ts
+++ src/post/dto/createPost.dto.ts
@@ -1,13 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreatePostDTO {
 
+    @ApiProperty({
+        description: 'Id of post creator',
+        type: Number
+    })
     @IsNotEmpty({ message: 'Post must have a userId. '})
     userId: number;
 
+    @ApiProperty()
     @IsNotEmpty({ message: 'Post must have a name.'})
     title: string;
 
+    @ApiProperty()
     @IsNotEmpty({ message: 'Post must have a description'})
     content: string;    
 }
