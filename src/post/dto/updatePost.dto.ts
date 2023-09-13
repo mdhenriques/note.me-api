0d5c1e87ab3d31f9e6e78865c4746beed7d0dd14
mdhenriques <md.henriques@outlook.com>
2023-09-13 17:22:05 -0300

    Adding ApiProperty to UpdatePostsDTO

--- src/post/dto/updatePost.dto.ts
+++ src/post/dto/updatePost.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class UpdatePostDTO {
         
+    @ApiProperty({
+        description: 'Posts title',
+        type: String
+    })
     @IsNotEmpty({ message: 'Post must have a name.'})
     title: string;
 
+    @ApiProperty({
+        description: 'Posts content',
+        type: String
+    })
     @IsNotEmpty({ message: 'Post must have a description'})
     content: string;   
 }
