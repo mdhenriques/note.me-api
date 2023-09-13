344cfca38627467b51b8cff19d6653f8b6514a95
mdhenriques <md.henriques@outlook.com>
2023-09-13 17:37:51 -0300

    Adding ApiProperty to User entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,28 +1,45 @@
 
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Table, Model } from "sequelize-typescript";
 
 
 @Table
 export class User extends Model {    
 
+    @ApiProperty({
+        description: 'Username',
+        type: Number
+    })
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     username: string;
 
+    @ApiProperty({
+        description: 'Users email',
+        type: String
+    })
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     email: string;
 
+    @ApiProperty({
+        description: 'Users password',
+        type: String
+    })
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     password: string;
 
+    @ApiProperty({
+        description: 'Property responsible for checking if the user is an Admin',
+        type: Boolean
+    })
     @Column({
         type: DataType.BOOLEAN,
         allowNull: false,
