4fc6b13f88c51698e82a32f3e563be62434bed1b
mdhenriques <md.henriques@outlook.com>
2023-09-13 17:35:08 -0300

    Adding ApiProperty to CreateUserDTO

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,14 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateUserDTO {
 
-    
+    @ApiProperty({
+        description: 'Username',
+        type: String
+    })
     @IsNotEmpty({ message: 'Username is required.'})
     username: string;
 
+    @ApiProperty({
+        description: 'Users email',
+        type: String
+    })
     @IsNotEmpty({ message: 'Email is required.'})
     email: string;
 
+    @ApiProperty({
+        description: 'Users password',
+        type: String
+    })
     @IsNotEmpty({ message: 'Password is required.'})
     password: string;
 }
